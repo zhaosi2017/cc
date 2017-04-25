c2f5d26592595d1f87854ee97dad914698c433fe
zhaosi2017 <zhaosi2017@gmail.com>
2017-04-25 13:38:27 +0700

    ignore config file

--- config/db.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-return [
-    'class' => 'yii\db\Connection',
-    'dsn' => 'mysql:host=127.0.0.1;dbname=cc',
-    'username' => 'root',
-    'password' => '111111',
-    'charset' => 'utf8',
-];
