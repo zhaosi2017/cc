c4e48ea9ddedc57e85a82cb5bb19e1c8d450185a
zhaosi2017 <zhaosi2017@gmail.com>
2017-04-25 13:59:32 +0700

    Delete db.php

--- config/db.php
+++
@@ -1,8 +0,0 @@
-<?php
-return [
-    'class' => 'yii\db\Connection',
-    'dsn' => 'mysql:host=127.0.0.1;dbname=callcenter',
-    'username' => 'root',
-    'password' => 'OASystem@Yii2+H+',
-    'charset' => 'utf8',
-];
