de604f241ee071dba6892351dcc3a7707873f0c7
zp4314 <zp4314@gmail.com>
2017-06-27 14:42:19 +0700

    防骚扰update

--- modules/home/views/user/index.php
+++ modules/home/views/user/index.php
@@ -24,18 +24,18 @@ $this->params['breadcrumbs'][] = $this->title;
                         <p class="m-t-lg">昵称：<?php echo $model->nickname; ?><a href="<?php echo Url::to(['/home/user/set-nickname']) ?>" class="btn btn-primary btn-sm pull-right"><?php echo $model->nickname ? '修改' : '去设置'?></a></p>
                         <p class="m-t-lg">白名单开关：<?php echo $model->whitelist_switch ? '开':'关' ;?></p>
 
-                        <p class="m-t-lg">防骚扰：
-                         <span title="<?php echo $model->un_call_number.('(总数)  ');echo $model->un_call_by_same_number.('(同一人)  ');echo $model->long_time.('(时间)');?>"  alt="<?php echo $model->un_call_number.('(总数)  ');echo $model->un_call_by_same_number.('(同一人)  ');echo $model->long_time.('(时间)');?>" style="    overflow: hidden;
-    text-overflow: hidden;
-    white-space: nowrap;
-    word-break: keep-all;
-    width: 160px;
-    max-width: 160px;
-    display: inline-block; " ><?php echo $model->un_call_number.('(总数)  ');echo $model->un_call_by_same_number.('(同一人)  ');echo $model->long_time.('  (时间)');?></span>
-                         <a href="<?php echo Url::to(['/home/user/harassment']) ?>" class="btn btn-primary btn-sm pull-right"><?php echo $model->un_call_number ? '修改' : '去设置'?>
-                             
-                         </a>
-                         </p>
+<!--                        <p class="m-t-lg">防骚扰：-->
+<!--                         <span title="--><?php //echo $model->un_call_number.('(总数)  ');echo $model->un_call_by_same_number.('(同一人)  ');echo $model->long_time.('(时间)');?><!--"  alt="--><?php //echo $model->un_call_number.('(总数)  ');echo $model->un_call_by_same_number.('(同一人)  ');echo $model->long_time.('(时间)');?><!--" style="    overflow: hidden;-->
+<!--    text-overflow: hidden;-->
+<!--    white-space: nowrap;-->
+<!--    word-break: keep-all;-->
+<!--    width: 160px;-->
+<!--    max-width: 160px;-->
+<!--    display: inline-block; " >--><?php //echo $model->un_call_number.('(总数)  ');echo $model->un_call_by_same_number.('(同一人)  ');echo $model->long_time.('  (时间)');?><!--</span>-->
+<!--                         <a href="--><?php //echo Url::to(['/home/user/harassment']) ?><!--" class="btn btn-primary btn-sm pull-right">--><?php //echo $model->un_call_number ? '修改' : '去设置'?>
+<!--                             -->
+<!--                         </a>-->
+<!--                         </p>-->
 
                         <p class="m-t-lg" style="margin-top: 43px;">登录密码：******<a href="<?php echo Url::to(['/home/user/password']) ?>" class="btn btn-primary btn-sm pull-right">去修改</a></p>
 
