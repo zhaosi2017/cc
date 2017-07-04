b9219b052d2d9684b8ddb4551ac817ccb3738acb
zp4314 <zp4314@gmail.com>
2017-07-04 18:03:45 +0700

    udpate

---
+++ messages/zh-TW/potato.php
@@ -0,0 +1,15 @@
+<?php return array (
+  'Start the operation, please wait later.' => '開始操作, 請稍後.',
+  'welcome' => '歡迎',
+  'Share your contact card' => '分享自己名片',
+  '[Please enter the verification code on the callu platform to complete the binding operation!]' => '[請在callu平台輸入該驗證碼, 完成綁定操作!]',
+  'Please share your own contact card to the robot, complete the binding operation.' => '請先分享自己的名片到機器人，完成綁定操作.',
+  'success' => '成功',
+  'failure' => '失敗',
+  'call' => '呼叫',
+  'You have completed the binding operation.' => '您已經完成了綁定操作.',
+  'is not a member of our system, can not perform the operation.' => '不是我們系統會員，不能執行該操作.',
+  'The number of times the call has exceeded the limit set by he.' => '呼叫超出本人設置的限制次數.',
+  'The verification code is empty.' => '驗證碼為空.',
+  'Verification code error.' => '驗證碼錯誤.',
+); 
