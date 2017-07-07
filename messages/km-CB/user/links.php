5fb7afd2f9c2ece6c6fa9cb49b9158160c7691e4
qingzhang931 <qingzhang931@gmail.com>
2017-07-07 14:06:18 +0700

    fix

--- messages/km-CB/user/links.php
+++ messages/km-CB/user/links.php
@@ -2,7 +2,7 @@
   'Contact information' => 'ទំនាក់ទំនង',
   'Personal Contact' => 'ទំនាក់ទំនងផ្ទាល់ខ្លួន',
   'Emergency Contact' => 'លេខ​ទំនាក់ទំនង​បន្ទាន់',
-  'Build' => 'Build',
+  'Build' => 'ការបន្',
   'Contact' => 'ទំនក់ទំនង',
   'Edit' => 'កែសម្រួល',
   'Remove' => 'យក',
