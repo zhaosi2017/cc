31502752580ed9b40601a0be35d0c2f0989e6646
qingzhang931 <qingzhang931@gmail.com>
2017-07-08 11:15:14 +0700

    fix

--- modules/home/views/user/add-urgent-contact-person.php
+++ modules/home/views/user/add-urgent-contact-person.php
@@ -29,48 +29,55 @@ $this->params['breadcrumbs'][] = $this->title;
     <?php echo $form->field($model, 'contact_nickname',[
         'template' => "<div>
                             <div style=\"display：inline-block;\">{label}</div>\n
-                            <div class=\"\" style=\"display:inline-block;     width: 245px;\">{input}</div>
+                            <div class=\"\" style=\"display:inline-block; margin-left: 10px;    width: 245px;\">{input}</div>
                             <div style=\"display:inline-block;margin-left:10px;\">
                                 <span >*".Yii::t('app/user/add-urgent-contact-person','Please enter the name of emergency contact.')."
                                 <span>
                             </div>\n
-                            <div><span class=\"help-block m-b-none\" style=\"margin-left:17%;\">{error}</span></div>",
+                            <div>
+                                <span class='col-sm-2' ></span>
+                                <span class=\"help-block m-b-none col-sm-4 \" style=\"margin-left:-5px;\">{error}</span>
+                            </div>
+                       </div>     ",
     ])->textInput(['placeholder' => Yii::t('app/user/add-urgent-contact-person','Emergency contact nickname')])
-    ->label(Yii::t('app/user/add-urgent-contact-person','Name') ,['style'=>"    text-align: left;padding-left: 100px; padding-top:7px"])
+    ->label(Yii::t('app/user/add-urgent-contact-person','Name') ,['style'=>"text-align:right; padding-top:7px"])
 
     ?>
 
-    <div class="row form-inline">
+    <div class="row form-inline" style="margin-bottom: 15px">
 
 
 
-        <div class="col-sm-12">
-           <!--  <div class="form-group">
-                <div class="col-sm-1">
-                    +
-                    <div class="help-block"></div>
-                </div>
-            </div> -->
-            <div class="col-sm-2 text-center" style="    font-weight: 600;">
-                <?=  Yii::t('app/user/add-urgent-contact-person','Phone Number')?></label>
+        <div class="col-sm-12" style="margin-left: -15px">
 
+            <div class="col-sm-2 text-right" style=" font-weight: 600;">
+                   <label style="margin-top: 7px"> <?=  Yii::t('app/user/add-urgent-contact-person','Phone Number')?></label>
             </div>
-            <?php echo $form->field($model, 'contact_country_code', [
+            <div class="col-sm-1">
+                <?php echo $form->field($model, 'contact_country_code', [
 
-                 'template' => "{label}\n<div style=\"width:130px;\">&nbsp;&nbsp;&nbsp;&nbsp;{input}\n
-                                            <span style=\"height:18px;\" class=\"help-block m-b-none\">{error}</span>
-                                         </div>",
-            ])->textInput(['size' => 5,'placeholder'=>Yii::t('app/user/add-urgent-contact-person','Country Code'),])
-             ->label(false) ?>
+                     'template' => "{label}\n<div class='col-sm-12' >{input}\n
+                                                <span  class=\"help-block m-b-none\">{error}</span>
+                                             </div>",
+                ])->textInput(['size' => 5,'placeholder'=>Yii::t('app/user/add-urgent-contact-person','Country Code'),])
+                 ->label(false) ?>
+            </div>
+            <div class="col-sm-9" style="margin-left: -15px">
+                <?php echo $form->field($model, 'contact_phone_number',[
 
-            <?php echo $form->field($model, 'contact_phone_number',[
+                    'template' => "{label}\n
+                                        <div class='col-sm-12'>&nbsp;{input}
+                                            <span style=\"margin-left:15px;\">*".
+                                                Yii::t('app/user/add-urgent-contact-person','Please enter the phone number of emergency contact.')
 
-                'template' => "<div>{label}\n<div>&nbsp;{input}<span style=\"margin-left:15px;\">*".
-                    Yii::t('app/user/add-urgent-contact-person','Please enter the phone number of emergency contact.')
+                                            ."</span>
+                                            <span  class=\"help-block m-b-none \">{error}</span>
+                                        </div>\n
+                                        
+                                   ",
 
-            ."</span></div>\n<span style=\"height:18px;\" class=\"help-block m-b-none\">{error}</span></div>",
-
-            ])->textInput(['placeholder' => Yii::t('app/user/add-urgent-contact-person','Emergency contact phone'), 'size'=>'17'])->label(false) ?>
+                ])->textInput(['placeholder' => Yii::t('app/user/add-urgent-contact-person','Emergency contact phone'), 'size'=>'16'])->label(false) ?>
+            </div>
         </div>
 
     </div>
@@ -91,9 +98,3 @@ $this->params['breadcrumbs'][] = $this->title;
 </div>
 
 
-<?php echo '<style type="text/css">
-    .has-error{
-        margin-bottom: 0px;
-    }
-</style>';
-?>
