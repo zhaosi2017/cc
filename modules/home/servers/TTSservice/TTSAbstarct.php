aa9227108ad9944d45646b33bbf1eb717137f1c0
zhangqing <qingzhang931@gmail.com>
2017-07-18 17:32:46 +0700

    fix

--- modules/home/servers/TTSservice/TTSAbstarct.php
+++ modules/home/servers/TTSservice/TTSAbstarct.php
@@ -9,19 +9,45 @@ namespace app\modules\home\servers\TTSservice;
 
 abstract  class TTSAbstarct{
 
-    public $voice ;   //语音播报声音
-    public $Language; //使用的语言
+    public $voice  ;          //语音播报声音
+    public $Language;         //使用的语言
+    public $loop;             //重复播报次数
 
-    public $messageText; //消息内容
-    public $messageType; //消息类型  短信 语音播报
+    public $messageText;      //消息内容
+    public $messageText_more; //第二次发送的消息 当有更多的联系电话时 消息可以设置
+    public $messageType;      //消息类型  短信 语音播报
+    public $messageId;        //发送的消息的id
+    public $messageStatus;    //发送状态
+    public $messageAnwser;    //消息应答
+    public $duration;         //通话时长
 
-    public $messageStatus; //发送状态
 
-    public $messageId;    //发送的消息的id
+    public $to;
+    public $from;
+
+    public $error;
 
 
-    abstract public function sendMessage();   //发送消息
-    abstract public function event();         //处理回调数据
+
+
+    /**
+     * @return bool
+     *
+     */
+    abstract public function sendMessage();         //发送消息
+
+    /**
+     * @param $event_data
+     * @return mixed
+     */
+    abstract public function event($event_data);         //处理回调数据
+
+
+
+    public function setError($error){
+            $this->error = $error;
+    }
+
 
 
 }
