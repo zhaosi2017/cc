0261ac9533a46afdc3e220b0bef33513668fdb2f
zp4314 <zp4314@gmail.com>
2017-07-19 11:11:45 +0700

    Update

--- messages/zh-CN/telegram.php
+++ messages/zh-CN/telegram.php
@@ -1,10 +1,5 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: nengliu
- * Date: 2017/7/1
- * Time: 上午11:25
- */
+
 return [
     'He is not a member of our system and you can not perform this operation.' => '他/她不是我们系统会员，不能执行该操作.',
     'Please share your own contact card to the robot, complete the binding operation.' => '请先分享自己的名片到机器人，完成绑定操作.',
