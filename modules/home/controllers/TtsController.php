66fa6dfb9d8462f7e0d00f2c3b7e2752d010fd8c
zhangqing <qingzhang931@gmail.com>
2017-07-19 17:32:04 +0700

    fix

--- modules/home/controllers/TtsController.php
+++ modules/home/controllers/TtsController.php
@@ -29,6 +29,7 @@ class TtsController extends GController{
 
 
         $callback_data = Yii::$app->request->post();
+        file_put_contents('/tmp/test_telegram.log' , var_export($callback_data ,true));
         $service = TTSservice::init(Sinch::class);
         $rest = $service->event($callback_data);
         echo $rest;
