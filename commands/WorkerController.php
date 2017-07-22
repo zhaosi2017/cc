2f17538f61eefff077fa83fbd614e6a34bd71f77
neng liu <nengliu@nengdeMacBook-Pro.local>
2017-07-22 15:24:13 +0700

    up

--- commands/WorkerController.php
+++ commands/WorkerController.php
@@ -12,4 +12,21 @@ class WorkerController extends BeanstalkController
         return ["tube"];
     }
 
+    public function actionTube($job){
+        $sentData = $job->getData();
+        try {
+            // something useful here
+            if($everthingIsAllRight == true){
+                fwrite(STDOUT, Console::ansiFormat("- Everything is allright"."\n", [Console::FG_GREEN]));
+                return self::DELETE; //Deletes the job from beanstalkd
+            }
+            fwrite(STDOUT, Console::ansiFormat("- Not everything is allright!!!"."\n", [Console::FG_GREEN]));
+            return self::DELAY; //Delays the job for later try
+            // if you return anything else job is released.
+        } catch (\Exception $e) {
+            //If there is anything to do.
+            fwrite(STDERR, Console::ansiFormat($e."\n", [Console::FG_RED]));
+            return self::DELETE;
+        }
+    }
 }
