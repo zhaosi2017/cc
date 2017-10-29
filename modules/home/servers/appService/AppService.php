0c25592d626e8c59d711f476ff5deeda87f68fe1
zhangqing <qingzhang931@gmail.com>
2017-10-29 09:21:37 +0700

    fix

--- modules/home/servers/appService/AppService.php
+++ modules/home/servers/appService/AppService.php
@@ -8,7 +8,43 @@
  */
 namespace app\modules\home\servers\appService;
 
+use app\modules\home\models\User;
+use app\modules\home\models\UserBindApp;
+
 class AppService{
+    /**
+     * @param $app_userid appid
+     * @param $app_type   app类型
+     * @return bool|static
+     */
+    public static function getUserByApp($app_userid , $app_type){
+        $app_name = UserBindApp::$APP_TYPE_MAPS[$app_type];
+        if(empty($app_name)){
+            return false;
+        }
+        $c_user_id =  $app_name.'_user_id';
+        $c_number = $app_name.'_number';
+        $c_country_code = $app_name.'_country_code';
+        $c_name   = $app_name.'_name';
+
+        $user = User::findOne([$c_user_id=>$app_userid]);
+        if(empty($user)){
+            $app = UserBindApp::findOne(['app_userid'=>$app_userid , 'type'=>$app_type]);
+            if(empty($app)){
+                return false;
+            }
+            $user = User::findOne(['id'=>$app->user_id]);
+            if(empty($user)){
+                $app->delete();
+                return false;
+            }
+            $user->$c_user_id = $app->app_userid;
+            $user->$c_number  = $app->app_number; //含国码
+            $user->$c_country_code  = '';
+            $user->$c_name  = $app->app_name;
+            return $user;
+        }
+    }
 
 
 
