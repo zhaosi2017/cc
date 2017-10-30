1c1a1da37d4aab3bd80ec060f5a3abf3d76a65e8
zhangqing <qingzhang931@gmail.com>
2017-10-30 16:13:50 +0700

    fix

--- modules/home/servers/TTSservice/TTSservice.php
+++ modules/home/servers/TTSservice/TTSservice.php
@@ -365,8 +365,8 @@ class TTSservice{
         $this->call_type            = $send['call_type'];
 
         $call_array['nickname'] =  $send['nickname'];
-        $call_array['link']     =  $send['link'];
-       // file_put_contents('/tmp/test_error.log' , var_export($call_array , true).PHP_EOL , 8);
+        $call_array['link']     =  isset($send['link'])?$send['link']:false;
+        file_put_contents('/tmp/test_error.log' , var_export($call_array , true).PHP_EOL , 8);
         $this->app_obj->continueCall($this->call_type ,$call_array );
         $result = $this->third->sendMessage();                                  //发送一个新的消息
 
